b5cc79284f3dcd42d9edea9d75e92ef95e990cf5
STENUNGSUND\lena.fridlund <lena.fridlund@hotmail.com>
2017-09-21 12:55:08 +0200

    New user class.

--- MyMovieDataBase/User.cs
+++ MyMovieDataBase/User.cs
@@ -7,5 +7,9 @@ namespace MyMovieDataBase
 {
     public class User
     {
+        public Guid UserID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }       
+
     }
 }
