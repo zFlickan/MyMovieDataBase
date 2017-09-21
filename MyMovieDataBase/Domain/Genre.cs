2850f2e56b51b7c55e1ff6d330aa7f84f9940e41
STENUNGSUND\frida.eriksson <frida_eriksson@live.se>
2017-09-21 10:25:07 +0200

    Added classes for Movie and Genre

---
+++ MyMovieDataBase/Domain/Genre.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyMovieDataBase.Domain
+{
+	class Genre
+	{
+		public Genre()
+		{
+			Movies = new List<Movie>();
+		}
+		public virtual int GenreID { get; set; }
+		public virtual string GenreName { get; set; }
+
+		public virtual ICollection<Movie> Movies { get; set; }
+	}
+
+	enum Genres
+	{
+		Action, Horror, Love, Drama, Biography, Thriller, War
+	}
+}
---
+++ MyMovieDataBase/Domain/Movie.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyMovieDataBase.Domain
+{
+	class Movie
+	{
+		public Movie()
+		{
+			Genres = new List<Genre>();
+		}
+		public virtual Guid MovieID { get; set; }
+		public virtual string Title { get; set; }
+
+		public virtual int Year { get; set; }
+		//public virtual int seenID { get; set; }
+		public virtual string Rating { get; set; }
+		public virtual string Link { get; set; }
+
+		public virtual ICollection<Genre> Genres { get; set; }
+
+		//public string genres
+		//{
+		//	get
+		//	{
+		//		string sql = $"Select Genres.GenreName from Genres where GenreID={genreID};";
+		//		return ReadFromDatabase.OpenSQLstatement(sql);
+		//	}
+
+		//}
+
+		//public string seen
+		//{
+		//	get
+		//	{
+		//		string sql = $"Select Seen.SeenVal from Seen where SeenID={seenID};";
+		//		return ReadFromDatabase.OpenSQLstatement(sql);
+		//	}
+		//}
+
+		//Constructor
+		//public Movie(string inputTitle, int inputYear, int inputGenre, int inputSeen, string inputRating, string inputLink)
+		//{
+		//	this.title = inputTitle;
+		//	this.year = inputYear;
+		//	this.genreID = inputGenre;
+		//	this.seenID = inputSeen;
+		//	this.rating = inputRating;
+		//	this.link = inputLink;
+		//}
+
+		//Empty Constructor
+		//public Movie()
+		//{
+
+		//}
+	}
+}
